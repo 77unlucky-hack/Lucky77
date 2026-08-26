4bee6f72207b0a33e83d370528cf2bfc9a76c1f8
77unlucky-hack <cekcprince@gmail.com>
2026-08-26 16:24:00 +0300

    Add Lucky77Theme interface for UI styling

---
+++ Sources/Lucky77Theme.h
@@ -0,0 +1,19 @@
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Lucky77Theme : NSObject
++ (UIColor *)background;
++ (UIColor *)panel;
++ (UIColor *)panelAlt;
++ (UIColor *)border;
++ (UIColor *)textPrimary;
++ (UIColor *)textSecondary;
++ (UIColor *)purple;
++ (UIColor *)purpleDark;
++ (UIColor *)purpleGlow;
++ (UIFont *)titleFont:(CGFloat)size;
++ (UIFont *)bodyFont:(CGFloat)size;
+@end
+
+NS_ASSUME_NONNULL_END
