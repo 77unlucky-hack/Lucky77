b82c58a187b9e4934a09baaf65857c05cd960dfb
77unlucky-hack <cekcprince@gmail.com>
2026-08-26 19:18:01 +0300

    Update Lucky77Menu.h

--- Sources/Lucky77Menu.h
+++ Sources/Lucky77Menu.h
@@ -5,12 +5,16 @@ NS_ASSUME_NONNULL_BEGIN
 FOUNDATION_EXPORT void Lucky77PresentMenu(UIViewController *presenter);
 FOUNDATION_EXPORT UIViewController *Lucky77CreateMenuViewController(void);
 
-// Глобальные флаги для функций чита
+// Глобальные флаги
 extern BOOL g_espEnabled;
 extern BOOL g_aimbotEnabled;
 extern BOOL g_radarHack;
 extern BOOL g_noRecoil;
 extern BOOL g_unlimitedAmmo;
 extern BOOL g_triggerbot;
+extern NSInteger g_fpsLimit;
+
+// Язык меню (0 - English, 1 - Русский)
+extern NSInteger g_language;
 
 NS_ASSUME_NONNULL_END
